ac4a4f305670002491d49abcdb652ac63f2c162f
Zaycev MA <solynkaz@yandex.ru>
2022-09-12 20:09:47 +0800

    Три поиска - Done, задебажено

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,109 +1,95 @@
+import java.util.*
 import kotlin.system.measureNanoTime
-import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     var initialList = mutableListOf<Int>()
-    println("Введите кол-во элементов")
-    var n = readLine()!!.toInt()
-    for (i in 0 until n!!) {
-        initialList.add(readLine()!!.toInt())
+    var n = 0
+    val sc = Scanner(System.`in`)
+    try {
+
+        print("Введите кол-во элементов: ")
+        n = sc.nextInt()
+        for (i in 0 until n) {
+            initialList.add(sc.nextInt())
+        }
+    } catch (exc: java.lang.NumberFormatException) {
+        println("Exception, $exc")
     }
     initialList.sort()
     while (true) {
-        println("Выберите из списка:\n1. Последовательный поиск\n2. Быстрый последовательный поиск\n3. Бинарный поиск\n4. Закончить")
-        var choice = readLine()!!.toInt()
-        println("Введите искомый элемент")
-        var itemToFind = readLine()!!.toInt()
+        println("Выберите из списка:\n1. Последовательный поиск\n2. Быстрый последовательный поиск\n3. Бинарный поиск\n4. Вывести массив\n5. Закончить")
+        var choice = sc.nextInt()
 
         when (choice) {
             //Линейный поиск
             1 -> {
+                println("Введите искомый элемент")
+                val itemToFind = sc.nextInt()
                 val elapsedTime = measureNanoTime {
                     for ((i, k) in initialList.withIndex()) {
                         if (k == itemToFind) {
-                            println("Удача. Элемент $i найден, номер элемента в массиве ${k + 1}.")
+                            println("---------------------------\nУдача. Элемент $itemToFind найден, номер элемента в массиве ${i}.\n---------------------------")
                             break
                         }
-                        if (k == n - 1) println("Неудача. Искомый элемент не найден.")
-
+                        if (k == n - 1) println("---------------------------\nНеудача. Искомый элемент не найден.\n---------------------------")
                     }
                 }
                 println("TotalTime: $elapsedTime")
             }
 
             2 -> {
+                println("Введите искомый элемент")
+                val itemToFind = sc.nextInt()
+                val initialListCopy = initialList.toMutableList()
                 val elapsedTime = measureNanoTime {
-                    initialList.add(itemToFind)
-                    for ((i, k) in initialList.withIndex()) {
-                        if (i == itemToFind) {
+                    initialListCopy.add(itemToFind)
+                    for ((i, k) in initialListCopy.withIndex()) {
+                        if (k == itemToFind) {
                             if (i > n) {
-                                println("Неудача. Искомый элемент не найден.")
+                                println("---------------------------\nНеудача. Искомый элемент не найден.\n---------------------------")
                                 break
                             } else {
-                                println("Удача. Элемент $i найден, номер элемента в массиве ${k + 1}.")
+                                println("---------------------------\nУдача. Элемент $k найден, номер элемента в массиве ${i}.\n---------------------------")
                                 break
                             }
                         }
                     }
                 }
                 println("TotalTime: $elapsedTime")
-
             }
-
             3 -> {
-                //Сортировка
-                var left = 0
-                var right = initialList.size
-                val elapsedTime = measureNanoTime {
-                    var midd = 0
-                    var left = 0
-                    right = n
-                    while (left <= right) {
-                        midd = (left + right) / 2
-                        if (itemToFind < initialList[midd]) right = midd - 1
-                        else if (itemToFind > initialList[midd])
-                            left = midd + 1
-                        else {
-                            println("Удача. Позиция: ${midd+1}")
+                try {
+                    println("Введите искомый элемент")
+                    val itemToFind = sc.nextInt()
+                    val elapsedTime = measureNanoTime {
+                        var index = -1;
+                        var low = 0
+                        var high = initialList.size
+                        while (low <= high) {
+                            var mid = (low + high) / 2
+                            if (initialList[mid] < itemToFind) {
+                                low = mid + 1;
+                            } else if (initialList[mid] > itemToFind) {
+                                high = mid - 1;
+                            } else if (initialList[mid] == itemToFind) {
+                                index = mid;
+                                break;
+                            }
                         }
-                        if (left > right) {
-                            println("Неудача")
-                            break;
-                        }
+                        if (low > high) {
+                        println("---------------------------\nНеудача.\n---------------------------")
+                    } else println("---------------------------\nУспех: Элемент найден с индексом: $index\n---------------------------")
                     }
+                    println("TotalTime: $elapsedTime\n")
+                } catch (exc: java.lang.Exception) {
+                    println("Неудача")
                 }
-                println("TotalTime: $elapsedTime")
             }
-/*
-https://pastebin.com/2UGBP7nu
- */
-
-            /*
-            int midd = 0, left = 0, right = N;
-	while (left <= right)
-	{
-		midd = (left + right) / 2;
-
-		if (key < mass[midd])
-			right = midd - 1;
-		else if (key > mass[midd])
-			left = midd + 1;
-		else
-		{
-			cout << "Удача. Позиция: " << midd + 1;
-			break;
-		}
-
-		if (left > right)
-		{
-			cout << "Неудача.";
-			break;
-		}
-	}
-
-             */
-
-            4 -> {
+            4-> {
+                for (i in initialList) print("$i ")
+                println()
+            }
+            5-> {
                 return
             }
         }
--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,6 +1,11 @@
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
+    var list1 = mutableListOf(1,2,3)
+    var list2 = list1.toMutableList()
 
+    println("DO: ${list1[0]}")
+    list2[0] = 10
+    println("POSLE: ${list1[0]}")
 
 }
